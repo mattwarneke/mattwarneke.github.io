df81ee7f3668dd9631f8dadcb0229dfa101eed75
Matt Warneke <warneke.matt@gmail.com>
2019-06-02 22:05:45 -0700

    Updates to both games - fix errors and menu. Redo button styles and icon styles of site.

---
+++ precache-manifest.b30eabaa4018364cbc241c468a4af6e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c895eb035a6739edf9f8225198c5777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed4bfa1021411833ab92",
+    "url": "/static/css/main.1ff278b2.chunk.css"
+  },
+  {
+    "revision": "decb8dd3e90eaccc0d6c",
+    "url": "/static/js/2.d9c782fc.chunk.js"
+  },
+  {
+    "revision": "ed4bfa1021411833ab92",
+    "url": "/static/js/main.d23e3e6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79455f07bbdf0c4163c63044b472e215.js"
+  "/precache-manifest.b30eabaa4018364cbc241c468a4af6e0.js"
 );
 
 self.addEventListener('message', (event) => {
